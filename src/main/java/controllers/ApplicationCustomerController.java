0059638ad1a605194b76e9a0d8d6850cc21bc531
daviidgilb <davidgil.univ@gmail.com>
2019-01-15 17:22:01 +0100

    Actualización Applications y Credit Cards

--- src/main/java/controllers/ApplicationCustomerController.java
+++ src/main/java/controllers/ApplicationCustomerController.java
@@ -138,6 +138,7 @@ public class ApplicationCustomerController extends AbstractController {
 			result = new ModelAndView("customer/changeStatus");
 			result.addObject("application", application);
 			result.addObject("message", "operation.error");
+
 		} else {
 			try {
 				if (application.getStatus().equals(Status.ACCEPTED)) {
@@ -208,12 +209,15 @@ public class ApplicationCustomerController extends AbstractController {
 	public ModelAndView changeApplicationStatusWithCreditCard(@Valid int applicationId, @Valid CreditCard creditCard, BindingResult binding) {
 		ModelAndView result = null;
 
-		if (binding.hasErrors() || Long.toString(creditCard.getNumber()).length() != 16 || Integer.toString(creditCard.getExpirationMonth()).length() != 2 || Integer.toString(creditCard.getExpirationYear()).length() != 2
-			|| Integer.toString(creditCard.getCvvCode()).length() != 3 || !(Integer.toString(creditCard.getExpirationMonth()).startsWith("0") || Integer.toString(creditCard.getExpirationMonth()).startsWith("1"))) {
+		List<String> cards = this.configurationService.getConfiguration().getCardType();
+
+		if (binding.hasErrors() || Long.toString(creditCard.getNumber()).length() != 16 || Integer.toString(creditCard.getExpirationMonth()).length() > 2 || Integer.toString(creditCard.getExpirationYear()).length() > 2
+			|| Integer.toString(creditCard.getCvvCode()).length() != 3 || creditCard.getExpirationMonth() > 12) {
 			result = new ModelAndView("customer/creditCard");
 			result.addObject("applicationId", applicationId);
 			result.addObject("creditCard", creditCard);
-			result.addObject("message", "operation.error");
+			result.addObject("message", "creditCard.operation.error");
+			result.addObject("cards", cards);
 		} else {
 			try {
 				Application application = this.applicationService.findOne(applicationId);
@@ -226,11 +230,13 @@ public class ApplicationCustomerController extends AbstractController {
 				result = new ModelAndView("redirect:list.do");
 				result.addObject("fixUpTaskId", applicationSave.getFixUpTask().getId());
 				result.addObject("applicationId", applicationSave.getId());
+				result.addObject("cards", cards);
 			} catch (Throwable oops) {
 				result = new ModelAndView("customer/creditCard");
 				result.addObject("applicationId", applicationId);
 				result.addObject("creditCard", creditCard);
-				result.addObject("message", "operation.error");
+				result.addObject("message", "creditCard.operation.error");
+				result.addObject("cards", cards);
 			}
 		}
 
